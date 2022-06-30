5519af325b8541dd158b1ba41f4641ddf6034261
tunde blades <oluwasheyidavidmicheal@gmail.com>
2022-06-30 08:27:28 +0100

    calculator app

---
+++ calc.js
@@ -0,0 +1,123 @@
+// let calc,result,one,two,three,four,five,six,seven,eight,nine,zero,dot,plus,minus,multi,divide;
+
+// //  function getData(){
+//     one = document.querySelector(`.one`).onclick =1;
+//     two = document.querySelector(`.two`).onclick=2 ;
+//     three = document.querySelector(`.three`).onclick=3;
+//     four = document.querySelector(`.four`).onclick=4;
+//     five = document.querySelector(`.five`).onclick=5;
+//     six = document.querySelector(`.six`).onclick=6;
+//     seven = document.querySelector(`.seven`).onclick=7;
+//     eight = document.querySelector(`.eight`).onclick=8;
+//     document.querySelector(`.nine`).onclick = nine;
+//     plus = document.querySelector(`.plus`).onclick = 2;
+//     minus = document.querySelector(`.minus`).onclick =3;
+//     multi = document.querySelector(`.multi`).onclick =3;
+//     divide = document.querySelector(`.divide`).onclick =4;
+//     dot = document.querySelector(`.dot`).onclick =5;
+// // }
+
+// let arrNum = [
+//     document.querySelector(`.nine`).onclick = function(){
+//         result = document.querySelector(`.calc`).innerHTML = document.querySelector('.nine').innerHTML;
+//     },
+//     document.querySelector('.eight').onclick = function(){
+//         result = document.querySelector(`.calc`).innerHTML = document.querySelector('.eight').innerHTML;
+//     },
+//     document.querySelector('.seven').onclick = function(){
+//         result = document.querySelector(`.calc`).innerHTML = document.querySelector('.seven').innerHTML;
+//     },
+//     document.querySelector('.six').onclick = function(){
+//         result = document.querySelector(`.calc`).innerHTML = document.querySelector('.six').innerHTML;
+//     },
+//     document.querySelector('.five').onclick = function(){
+//         result = document.querySelector(`.calc`).innerHTML = document.querySelector('.five').innerHTML;
+//     },
+//     document.querySelector('.four').onclick = function(){
+//         result = document.querySelector(`.calc`).innerHTML = document.querySelector('.four').innerHTML;
+//     },
+//     document.querySelector('.three').onclick = function(){
+//         result = document.querySelector(`.calc`).innerHTML = document.querySelector('.three').innerHTML;
+//     },
+//     document.querySelector('.two').onclick = function(){
+//         result = document.querySelector(`.calc`).innerHTML = document.querySelector('.two').innerHTML;
+//     },
+//     document.querySelector('.one').onclick = function(){
+//         result = document.querySelector(`.calc`).innerHTML = document.querySelector('.one').innerHTML;
+//     },
+//     document.querySelector('.zero').onclick = function(){
+//         result = document.querySelector(`.calc`).value = document.querySelector('.zero').value;
+//     },
+//     document.querySelector('.dot').onclick = function(){
+//         result = document.querySelector(`.calc`).innerHTML = document.querySelector('.dot').innerHTML;
+//     }
+      
+// ]
+
+
+
+// let ondo = []; 
+// ondo.push(`akoko`, `akure`);
+// console.log(ondo);
+
+
+
+
+
+// for (let i = 0; i <= 100; i++) {
+//     if (i % !3 && i % !5) {
+//         console.log(i);
+        
+//     }
+//     else{
+//         console.log(`weeeeee`);
+        
+//     }
+    
+    
+// }
+
+
+// let arri = new Array();
+// let i = 0;
+// while (  i >= arri .length, i+=1) {
+//     let harsh = (arri.unshift(i));
+//     console.log(harsh);    
+//     if (i == 200) {
+//         continue;
+        
+//  }
+//  else if (i == 400) {
+//     break;
+    
+// }
+// }
+
+//  let names = `my javascript will javascript, in many javascript ways if the javascript javasript well`;
+
+// // let namesPattern = /\w[javascript][ ,]?/gi;
+// let result = names.match(/\w[javascript][ ,]?/gi);
+// console.log(result.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
